18f3c4233cf4eb776e8a8476cac91321c003373b
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-02-09 14:58:44 +0300

    Time: 2036 ms (37.34%), Space: 13.3 MB (49.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# Example usage
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))
+
+
+
+     
