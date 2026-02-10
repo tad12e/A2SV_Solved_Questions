f9030b532fd36b4a61997039adde22f0ef69dc64
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-02-10 14:33:46 +0300

    Time: 0 ms (100%), Space: 19.3 MB (55.51%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if(n==1 or n==7):
+            return True
+        elif(n<10):
+            return False
+        else:
+            sum =0
+            while(n>0):
+                temp = n%10
+                sum += temp*temp
+                n= n//10
+            return self.isHappy(sum) 
+        
+
+        
+       
+    
+                    
+            
+
+
+
+        
