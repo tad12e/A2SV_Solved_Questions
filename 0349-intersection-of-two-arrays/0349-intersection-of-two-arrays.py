a38623ceb3ad332123c939d9d500d6c53edb422b
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-02-13 11:50:21 +0300

    Time: 11 ms (15.57%), Space: 19.4 MB (55.32%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res=[]
+
+        for i in nums1:
+            if i in nums2:
+                if i not in res:
+                    res.append(i)
+        return res
+        
