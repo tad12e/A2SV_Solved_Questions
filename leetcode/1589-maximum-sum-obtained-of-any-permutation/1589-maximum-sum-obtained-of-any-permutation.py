3833bffa009df28bd9682a0b3843a769b24a9f89
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-03-04 22:13:39 +0300

    Add solution for Maximum Sum Obtained of Any Permutation

--- leetcode/1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ leetcode/1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -1,26 +1,62 @@
 class Solution:
-    def maxSumRangeQuery(self, nums, requests):
-        n = len(nums)
-        freq = [0] * n
-
-        # difference array
-        for start, end in requests:
-            freq[start] += 1
-            if end + 1 < n:
-                freq[end+1] -= 1
-
-        # build actual frequency
-        for i in range(1, n):
-            freq[i] += freq[i-1]
-
-        nums.sort()
-        freq.sort()
-
-        ans = 0
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
         MOD = 10**9 + 7
-        
-        for i in range(n):
-            ans = (ans + nums[i] * freq[i]) % MOD
 
-        return ans
+        prefix=[0]*len(nums)
+
+
+        for i in requests:
+            start, end=i
+
+            prefix[start]+=1
+            if end+1 < len(nums):
+                prefix[end+1]-=1
+        sm=0 
+        arr=[]
+        for i in prefix:
+            sm+=i
+            arr.append(sm)
+
+        #print(arr)
+        
+        perms=[0]*len(nums)
+        lis=[]
+        for i in range(len(arr)):
+            lis.append((i, arr[i]))
+    
+
+
+
+
+
+        nums.sort(reverse=True)
+        sor=sorted(lis, reverse=True, key=lambda x:x[1])
+        print(sor)
+        print(arr)
+
+
+        for i in range(len(nums)):
+            perms[sor[i][0]]=nums[i]
+
+        print(perms)
+
+
+        pre=[]
+        sm2=0
+        ans=0
+
+
+        for i in perms:
+            sm2+=i
+            pre.append(sm2)
+
+
+        for start, end in requests:
+            if start>0:
+                ans+=pre[end]-pre[start-1]
+            else:
+                ans+=pre[end]
+
+        print(pre)
+        return ans%MOD
 
