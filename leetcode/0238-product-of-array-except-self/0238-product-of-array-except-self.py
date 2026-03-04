a3711659a1131f46073cba6c7de95bb83b7e456b
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-03-04 18:36:00 +0300

    Add solution for Product of Array Except Self

--- leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,28 +1,12 @@
-# Here I have used division operation
-
-class Solution(object):
-    def productExceptSelf(self, nums):
-        total_product = 1
-        zero_count = 0
-        
-        for num in nums:
-            if num != 0:
-                total_product *= num
-            else:
-                zero_count += 1
-                
-        result = []
-        
-        if zero_count > 1:
-            return [0] * len(nums)
-        
-        for num in nums:
-            if num != 0:
-                if zero_count == 1:
-                    result.append(0)
-                else:
-                    result.append(total_product // num)
-            else:
-                result.append(total_product)
-        
-        return result
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length=len(nums)
+        sol=[1]*length
+        pre = 1
+        post = 1
+        for i in range(length):
+            sol[i] *= pre
+            pre = pre*nums[i]
+            sol[length-i-1] *= post
+            post = post*nums[length-i-1]
+        return(sol)
