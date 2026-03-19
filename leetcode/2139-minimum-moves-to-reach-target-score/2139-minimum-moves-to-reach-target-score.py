c0ea9cc1b9e9956d9f71b92ab38016c0a331e753
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-03-19 11:43:21 +0300

    Add solution for Minimum Moves to Reach Target Score

--- leetcode/2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
+++ leetcode/2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -1,63 +1,15 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
-        count=0
-        curr_num=1
-        arr=[]
-        num=target
-
-
-
-        if maxDoubles==0:
-            return target-curr_num
-        if target==curr_num:
-            return 0
-       
-
-        f=0
-        while maxDoubles>f and target>0:
-            if target%2==0:
-                arr.append([target//2,0])
-                target=target//2
+        ans = 0
+        while target>1:
+            if target%2==0 and maxDoubles>0:
+                target/=2
+                maxDoubles-=1
+                ans+=1
+            elif maxDoubles==0:
+                ans+=target-1
+                break
             else:
-                arr.append([(target-1)//2,1])
-                target=(target-1)//2
-            f+=1
-        
-
-        for i in arr[::-1]:
-            val, intf=i
-
-            if val==0:
-                continue
-
-            if intf==0:
-                count+=(abs(curr_num-val))+1
-                curr_num+=abs(curr_num-val)
-                curr_num=curr_num*2
-            else:
-                count+=(abs(curr_num-val))+1
-                curr_num+=abs(curr_num-val)
-                curr_num=curr_num*2
-        print(arr)
-        if num%2!=0:
-            return count+1
-        else:
-            return count
-        
-
-
-
-
-            
-        print(arr)
-        
-        
-
-
-       
-
-        return count+(target-curr_num)
-
-
-
-        
+                target-=1
+                ans+=1
+        return int(ans)
