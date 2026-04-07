91130712152972c9af9140339f936cc9c357206a
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-04-07 10:04:23 +0300

    Add solution for Next Greater Element I

--- leetcode/0496-next-greater-element-i/0496-next-greater-element-i.py
+++ leetcode/0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,29 +1,33 @@
+from typing import List
+
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans=[]
-        dect={}
-        f=len(nums2)
 
+        stack = []
+        res = [-1] * len(nums2)   
+
+     
+        for i in range(len(nums2)):
+
+            while stack and nums2[i] > nums2[stack[-1]]:
+                res[stack[-1]] = nums2[i]
+                stack.pop()
+
+            stack.append(i)
+
+        
+        next_map = {}
 
         for i in range(len(nums2)):
-            if nums2[i] in nums1:
+            next_map[nums2[i]] = res[i]
+        ans = []
 
-                dect[nums2[i]]=i
-               
-        for i in nums1:
-            if dect[i]+1<f:
-                
-                for k in nums2[dect[i]+1:]:
-                    if k>i:
-                        ans.append(k)
-                        break
-                
-                else:
-                    ans.append(-1)
-            else:
-                ans.append(-1)
-        print(dect)
-       
+        for num in nums1:
+            ans.append(next_map[num])
 
         return ans
-            
+        
+
+
+
+        
