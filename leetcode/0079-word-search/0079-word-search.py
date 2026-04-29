93bc53621228bfa63b362aefc256913c67815010
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-04-29 19:55:02 +0300

    Add solution for Word Search

---
+++ leetcode/0079-word-search/0079-word-search.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c, index):
+            if index == len(word):
+                return True
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != word[index]:
+                return False
+
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            found = (
+                dfs(r+1, c, index+1) or
+                dfs(r-1, c, index+1) or
+                dfs(r, c+1, index+1) or
+                dfs(r, c-1, index+1)
+            )
+
+            board[r][c] = temp
+            return found
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == word[0]:
+                    if dfs(row, col, 0):
+                        return True
+        return False
