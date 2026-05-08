350c08e0312205fb19b8c84bc243dbad4381795d
TADIWOS GETAHUN TAMENE <tadiwosgetahun49@gmail.com>
2026-05-08 19:57:06 +0300

    Add solution for Score of Parentheses

---
+++ leetcode/0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = [0]
+
+        for ch in s:
+            if ch == "(":
+                stack.append(0)
+            else:
+                val = stack.pop()
+
+                if val == 0:
+                    stack[-1] += 1
+                else:
+                    stack[-1] += 2 * val
+
+        return stack[0]
+
+
+
+            
+            
+            
+                    
+
+                    
+
+
+             
+        
+
+      
+        
+        
